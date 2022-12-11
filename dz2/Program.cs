f1a04dce1a2b243b042f20c5bf35f9e4fa8cd955
Igor <kipr29611@gmail.com>
2022-12-11 09:25:25 +0300

    2dz_code

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите любое число: ");
+string n = Console.ReadLine();
+Length[0];
+if(Length >= 3)
+{
+    System.Console.WriteLine(" Третья цифра : " + Length[2]);
+}
+else
+{
+      Console.WriteLine("Третьего числа нет");
+}
