b1c56fe3e3e939d355ea7a09c0f40b3694cb2ab1
Igor <kipr29611@gmail.com>
2022-12-12 14:04:28 +0300

    dz5_code

---
+++ dz5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++) 
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
+Console.WriteLine("\nТаблица кубов числа: " + num);
